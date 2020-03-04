ce84c537a123f5e44aca719827ffcfa9d613797e
sramkumar30 <59263774+sramkumar30@users.noreply.github.com>
2020-03-03 19:01:06 -0500

    Add files via upload
    Changes:
    1) Print date selected in calendar, time in time widget and text in task description upon clicking OK.
    2) Close the calendar window when clicked on cancel without printing any selection.
    3) Moved CalendarWindow class to another file and changed class name from calendarpopup to calendarwindow
    4) Removed unnecessary imports in FollowUp.py and CalendarWindow.py

---
+++ CalendarWindow.py
@@ -0,0 +1,66 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QCalendarWidget, QLineEdit, QTimeEdit
+from PyQt5.QtCore import QTime, QDate
+
+
+class CalendarWindow(QMainWindow):
+    def __init__(self):
+        super(CalendarWindow, self).__init__()
+        self.setGeometry(300, 300, 400, 400)
+        self.setWindowTitle("Calendar")
+        self.init_ui()
+
+    def init_ui(self):
+        self.init_calendar()
+        self.init_lineedit()
+        self.init_pushbuttons()
+        self.init_timedit()
+
+        self.show()
+
+    def init_calendar(self):
+        try:
+            self.date = QDate()
+            self.calendar = QCalendarWidget(self)
+            self.calendar.adjustSize()
+            self.calendar.setMinimumDate(self.date.currentDate())
+        except Exception as err:
+            print(err)
+
+    def init_lineedit(self):
+        self.text = QLineEdit(self)
+        self.text.setMaxLength(250)
+        self.text.setGeometry(5,310,40,40)
+        self.text.setFixedWidth(350)
+        self.text.setPlaceholderText("Enter Task Description")
+
+    def init_pushbuttons(self):
+        self.OK = QtWidgets.QPushButton(self)
+        self.OK.setText("OK")
+        self.OK.move(195, 360)
+        self.OK.clicked.connect(self.show_main)
+        self.Cancel = QtWidgets.QPushButton(self)
+        self.Cancel.setText("Cancel")
+        self.Cancel.move(295, 360)
+        self.Cancel.clicked.connect(self.close)
+
+    def init_timedit(self):
+        self.time = QTime()
+        self.tedit = QTimeEdit(self)
+        self.tedit.setTime(self.time.currentTime())
+        self.tedit.move(5, 260)
+
+
+    def clicked(self):
+        print("Clicked")
+
+    def show_main(self):
+        try:
+            print(self.calendar.selectedDate())
+            selected_time = self.tedit.time()
+            print(selected_time.toString())
+            print(self.text.text())
+            self.close()
+
+        except Exception as err:
+            print(err)
--- FollowUp.py
+++ FollowUp.py
@@ -1,27 +1,25 @@
 import sys
-from PyQt5 import QtWidgets, QtGui
-from PyQt5.QtWidgets import QApplication, QMainWindow, QCalendarWidget, QMessageBox, QLineEdit, QTimeEdit
-from PyQt5.QtCore import QTime
-
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from CalendarWindow import CalendarWindow
 
 
 class FollowUp(QMainWindow):
     def __init__(self):
-        super(FollowUp,self).__init__()
+        super(FollowUp, self).__init__()
         self.setGeometry(150, 150, 500, 300)
         self.setWindowTitle("FollowUp")
-        #self.showcalendar = CalendarPopUp()
+
         self.init_ui()
 
     def init_ui(self):
         self.add = QtWidgets.QPushButton(self)
         self.add.setText("Add")
         self.add.clicked.connect(self.show_calendar)
-        #self.add.clicked.connect(self.clicked)
 
         self.edit = QtWidgets.QPushButton(self)
         self.edit.setText("Edit")
-        self.edit.move(100,0)
+        self.edit.move(100, 0)
         self.edit.clicked.connect(self.clicked)
 
         self.delete = QtWidgets.QPushButton(self)
@@ -36,70 +34,7 @@ class FollowUp(QMainWindow):
         print("Clicked")
 
     def show_calendar(self):
-        self.calendarPopUp = CalendarPopUp()
-'''
-    def show_calendar(self):
-        msgbox = QMessageBox()
-        #msgbox = CalendarPopUp()
-        msgbox.setWindowTitle("Calendar")
-        msgbox.setStandardButtons(QMessageBox.Ok|QMessageBox.Cancel)
-        #msgbox.setGeometry(300, 300, 200, 200)
-        x = msgbox.exec_()
-        #msgbox.show()
-'''
-'''
-class CalendarPopUp(QDialog):
-    def __init__(self):
-        self._vbox = QVBoxLayout()
-        self._calendar = QCalendarWidget()
-        self._calendar.setWindowTitle("Calendar")
-        self._vbox.addWidget(self._calendar)
-        self.show()
-'''
-class CalendarPopUp(QMainWindow):
-    def __init__(self):
-        super(CalendarPopUp,self).__init__()
-        self.setGeometry(300, 300, 400, 400)
-        self.setWindowTitle("Calendar")
-        self.init_ui()
-
-    def init_ui(self):
-        self.init_calendar()
-        self.init_lineedit()
-        self.init_pushbuttons()
-        self.init_timedit()
-        self.show()
-
-    def init_calendar(self):
-        self.calendar = QCalendarWidget(self)
-        self.calendar.adjustSize()
-
-    def init_lineedit(self):
-        self.text = QLineEdit(self)
-        self.text.setMaxLength(250)
-        self.text.setGeometry(5,310,40,40)
-        self.text.setFixedWidth(350)
-        self.text.setPlaceholderText("Enter Task Description")
-
-    def init_pushbuttons(self):
-        self.OK = QtWidgets.QPushButton(self)
-        self.OK.setText("OK")
-        self.OK.move(195,360)
-        self.Cancel = QtWidgets.QPushButton(self)
-        self.Cancel.setText("Cancel")
-        self.Cancel.move(295, 360)
-
-    def init_timedit(self):
-        self.time = QTime()
-        self.tedit = QTimeEdit(self)
-        self.tedit.setTime(self.time.currentTime())
-        self.tedit.move(5,260)
-
-
-        #self.text.move(200,300)
-
-
-
+        self.calendar_window = CalendarWindow()
 
 def window():
     app = QApplication(sys.argv)
